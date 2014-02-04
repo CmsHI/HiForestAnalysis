b73d5e64aeb087375664d9bbd87546f2f4b89268
velicanu <velicanu@mit.edu>
2014-02-04 13:53:49 -0500

    Update SetupHitTree.h
    doCheck should not be off by default! there's no reason it should ever be off because if there is overflow it will write in other memory addresses!

--- SetupHitTree.h
+++ SetupHitTree.h
@@ -42,7 +42,7 @@ public :
 };
 
 
-void setupHitTree(TTree *t,Hits &tHits,bool doCheck = 0)
+void setupHitTree(TTree *t,Hits &tHits,bool doCheck = 1)
 {
    // Set branch addresses and branch pointers
    if (t->GetBranch("n")) t->SetBranchAddress("n", &tHits.n, &tHits.b_n);
