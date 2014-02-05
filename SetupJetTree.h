2186622604d580df1fd8d147175e797451153e90
velicanu <velicanu@mit.edu>
2014-02-05 07:22:04 -0500

    Update SetupJetTree.h
    doCheck should not be off by default! there's no reason it should ever be off because if there is overflow it will write in other memory addresses!

--- SetupJetTree.h
+++ SetupJetTree.h
@@ -175,7 +175,7 @@ public :
 };
 
 
-void setupJetTree(TTree *t,Jets &tJets,bool doCheck = 0)
+void setupJetTree(TTree *t,Jets &tJets,bool doCheck = 1)
 {
    // Set branch addresses and branch pointers
    if (t->GetBranch("evt")) t->SetBranchAddress("evt", &tJets.evt, &tJets.b_evt);
