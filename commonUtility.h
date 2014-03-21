1087caf658c2caa881b5ca1429fb4ac8a05b393e
velicanu <velicanu@mit.edu>
2014-03-21 01:52:23 -0400

    Update commonUtility.h
    added getDR and getDPHI from the gammaJetAnalysis commonUtility

--- commonUtility.h
+++ commonUtility.h
@@ -648,5 +648,27 @@ void stripErr(TH1* theHist=0) {
 }
 
 
+Double_t getDR( Double_t eta1, Double_t phi1, Double_t eta2, Double_t phi2){ 
+  Double_t theDphi = getDPHI( phi1, phi2);
+  Double_t theDeta = eta1 - eta2;
+  return TMath::Sqrt ( theDphi*theDphi + theDeta*theDeta);
+}
+
+
+Double_t getDPHI( Double_t phi1, Double_t phi2) {
+  Double_t dphi = phi1 - phi2;
+ 
+  if ( dphi > 3.141592653589 )
+    dphi = dphi - 2. * 3.141592653589;
+  if ( dphi <= -3.141592653589 ) 
+    dphi = dphi + 2. * 3.141592653589;
+  
+  if ( TMath::Abs(dphi) > 3.141592653589 ) {
+    cout << " commonUtility::getDPHI error!!! dphi is bigger than 3.141592653589 " << endl;
+  }
+  
+  return dphi;
+}
+
 
 #endif 
