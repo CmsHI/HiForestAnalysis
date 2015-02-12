2a890d4536e4bf6bae5923d4a32c485c0cfc493b
yetkinyilmaz <yetkin.yilmaz@cern.ch>
2015-02-12 15:11:58 +0100

    weighting macro updated for workshop

--- weightMix.C
+++ weightMix.C
@@ -54,38 +54,25 @@ static bool reweightCentrality = 1;
 static const double pi = TMath::Pi();
 
 
-void weightSample(
-		  bool mix = 1,
-		  bool PbPb2011 = 0,
-		  int entryType = 2
-		  ){
+void weightMix(){
 
 
   TH1::SetDefaultSumw2();
-  bool validate = 1;
-  //  validate = 0;
 
   int Npt = 9;
 
-  double pthatBins2013[] = {15,30,50,80,120,170,220,280,370,9999};
-  double xs2013[] = {5.335E-01,3.378E-02,3.778E-03,4.412E-04,6.147E-05,1.018E-05,2.477E-06,6.160E-07,1.088E-07};
-
-  double pthatBins2011[] = {15,30,50,80,100,120,170,200,250,300,9999};
-  double xs2011[] = {2.03E-01,1.079E-02,1.021E-03,9.913E-05,3.069E-05,1.128E-05,1.470E-06,5.310E-07,1.192E-07,3.176E-08};
+  // https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiForestPA2013#Dijet_Cross_Sections_for_reweigh
+  double pthatBins2011[] = {30,50,80,120,170,220,9999};
+  double xs2011[] = {1.075E-02,1.025E-03,9.865E-05,1.129E-05,1.465E-06,2.837E-07};
 
   double *xs;
   double *pthats;
   int nev[20];
   int n[20];
 
-  if(PbPb2011){
-    Npt = 10;
-    xs = xs2011;
-    pthats = pthatBins2011;
-  }else{
-    xs = xs2013;
-    pthats = pthatBins2013;
-  }
+  Npt = 6;
+  xs = xs2011;
+  pthats = pthatBins2011;
 
   for(int i = 0; i < Npt-1; ++i){
     xs[i] -= xs[i+1];
@@ -97,49 +84,48 @@ void weightSample(
   string ntwNames[] = {"ntw","ntjetw","nttrkw"};
   string ntevtNames[] = {"ntevt","ntjet","nttrk"};
 
-  if(mix){
-    if(PbPb2011){
-      outf = new TFile(Form("weights_hydjet_i%d.root",entryType),"recreate");
-      nt = new TChain(ntevtNames[entryType].data());
-    }else{
-      outf = new TFile(Form("weights_hijing_i%d.root",entryType),"recreate");
-      if(0){
-	nt = new TChain("ak5PFJetAnalyzer/t");
-      }else{
-	nt = new TChain(ntevtNames[entryType].data());
-      }
-    }
-  }else{
-    outf = new TFile(Form("weights_signal_i%d.root",entryType),"recreate");
-    nt = new TChain("ak5PFJetAnalyzer/t");
-  }
+  nt = new TChain("akPu3PFJetAnalyzer/t");
+  // Order of files matters a lot!
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat30/HiForest_103_1_1Kh.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat30/HiForest_104_1_HZZ.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat30/HiForest_105_1_UAy.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat30/HiForest_106_1_auT.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat30/HiForest_10_1_MBR.root");
 
-  TNtuple* ntw = new TNtuple(Form("%s",ntwNames[entryType].data()),"","weight:ptweight:cweight:varpthat");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat50/HiForest_100_1_SOu.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat50/HiForest_101_1_W2S.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat50/HiForest_102_1_thH.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat50/HiForest_104_1_vVK.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat50/HiForest_105_1_DZB.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat50/HiForest_10_1_S9B.root");
 
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat80/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_100_1_rEg.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat80/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_101_1_WDk.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat80/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_102_1_GIg.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat80/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_103_1_ZXv.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat80/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_104_1_0Jr.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat80/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_10_1_bxy.root");
+
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat120/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_101_1_Jvo.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat120/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_103_1_Jnf.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat120/hiForest_DijetpT370_Hydjet1p8_STARTHI53_LV1_v15_10_1_Ull.root");
+
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat170/HiForest_211_1_tWC.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat170/HiForest_32_1_8L1.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat170/HiForest_331_1_EFf.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat170/HiForest_336_1_Los.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat170/HiForest_343_1_BfL.root");
+
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat220/HiForest_100_1_R7V.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat220/HiForest_101_1_su9.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat220/HiForest_102_1_cMj.root");
+  nt->AddFile("/Users/yetkinyilmaz/analysis/data/mc/pthat220/HiForest_10_1_JKD.root");
+
+
+  outf = new TFile(Form("weights.root"),"recreate");
+  TNtuple* ntw = new TNtuple("weights","","weight:ptweight:cweight:varpthat");
   float varpthat, varbin;
-  //  TNtuple* nt = (TNtuple*)(new TFile("mix.root"))->Get("nt");
 
-
-  if(PbPb2011){
-    nt->AddFile("mix_hydjet.root");
-  }else{
-
-    if(0){
-      for(int ip = 0; ip < 6; ++ip){
-	int forestversion = 2;
-	if(ip  >= 5) forestversion = 1;	
-	if(mix){	  
-	  nt->AddFile(Form("root://eoscms//eos/cms/store/caf/user/dgulhan/pPb/HP03/prod09/Hijing_Pythia_pt%d/HiForest_v55_v04_merged01/pt%d_HP03_prod09_merged_forest_0.root",(int)pthats[ip],(int)pthats[ip]));	  	  
-	}else{      
-	  forestversion =1;
-	  nt->AddFile(Form("root://eoscms//eos/cms/store/caf/user/dgulhan/pPb_A03/Signal_Pythia%d/JEC_merged02/merged_forest_0.root",(int)pthats[ip]));      
-	}	
-      }
-    }else{
-      nt->AddFile("mix_hijing.root");
-    }
-
-  }
     
   nt->SetBranchAddress("pthat",&varpthat);
   if(0){
@@ -173,11 +159,4 @@ void weightSample(
 
 }
 
-void weightMix(int isample = 0){
-
-  weightSample(1,isample,2);
-  weightSample(1,isample,1);
-  weightSample(1,isample,0);
-
-}
 
